2a3776825744c13a38f8bcf04a0989a202aa6be8
Saksham-L <70284118+Saksham-L@users.noreply.github.com>
2026-04-19 18:08:37 -0700

    did work

--- SLM.py
+++ SLM.py
@@ -205,3 +205,59 @@ class GPT(nn.Module):
             idx_next = torch.multinomial(probs, num_samples=1)
             idx = torch.cat((idx, idx_next), dim=1)
         return idx
+config = GPTConfig(
+    vocab_size=50257,
+    block_size=128,       # here i have taken the context size of 128, i used 4 only for explanation
+    n_layer=6, # no of layer is the transformer blocks' number that u use...
+    n_head=6, #no of attention heads as in Multi Head Attention
+    n_embd=384, # The embedding dimension .... try chnaging thinhs in this code here and there
+    dropout=0.1,
+    bias=True
+)
+
+model = GPT(config)
+
+
+def estimate_loss(model):
+    out = {}
+    model.eval()
+    with torch.inference_mode():
+        for split in ['train', 'val']:
+            losses = torch.zeros(eval_iters)
+            for k in range(eval_iters):
+                X, Y = get_batch(split)
+                with ctx:
+                    logits, loss = model(X, Y)
+                losses[k] = loss.item()
+            out[split] = losses.mean()
+    model.train()
+    return out
+
+
+# Training Config
+import torch
+from contextlib import nullcontext
+
+learning_rate = 1e-4 #more stable training, earlier 1e-4
+max_iters = 10000 #increase from 25000
+warmup_steps = 1000 #smoother initial train, earlier 100
+min_lr = 5e-4 #lower rate, earlier 5e-4
+eval_iters = 500 # increased from 100
+batch_size = 32 # changed from 16, better gradient estimate
+block_size = 128 #changed from 64, capture longer range dependencies
+
+gradient_accumulation_steps = 32 # reduced from 50
+
+device =  "cuda" if torch.cuda.is_available() else "cpu"
+device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
+# note: float16 data type will automatically use a GradScaler
+
+# How to use autocast https://wandb.ai/wandb_fc/tips/reports/How-To-Use-Autocast-in-PyTorch--VmlldzoyMTk4NTky
+#dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
+dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
+ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
+
+ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+
+torch.set_default_device(device)
+torch.manual_seed(42)
